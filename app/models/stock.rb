8a50e82297e0f417cbd7c8fba283f10f2c0afd2d
Petervl80 <pedro.lapa1911@gmail.com>
2026-05-06 14:42:31 -0300

    Add method to lookup stock info

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,8 @@
 class Stock < ApplicationRecord
+
+    def self.new_lookup(ticker_symbol)
+        quote = Alphavantage::TimeSeries.new(symbol: ticker_symbol).quote
+        quote.price
+    end
+
 end
