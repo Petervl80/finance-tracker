0611d88178df1b2449d9b958ce1a811c85f39052
Petervl80 <pedro.lapa1911@gmail.com>
2026-05-08 10:33:18 -0300

    Use Ajax for submission

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,8 +1,17 @@
 class Stock < ApplicationRecord
 
-    def self.new_lookup(ticker_symbol)
-        quote = Alphavantage::TimeSeries.new(symbol: ticker_symbol).quote
-        quote.price
-    end
+	def self.new_lookup(ticker_symbol)
+		begin
+			quote_thread    = Thread.new { Alphavantage::TimeSeries.new(symbol: ticker_symbol).quote }
+			overview_thread = Thread.new { Alphavantage::Fundamental.new(symbol: ticker_symbol).overview }
+
+			quote    = quote_thread.value
+			overview = overview_thread.value
+
+			new(ticker: overview.symbol, name: overview.name, last_price: quote.price)
+		rescue => exception
+			return nil
+		end
+	end
 
 end
